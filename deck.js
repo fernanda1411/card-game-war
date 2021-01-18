14ca5065193b54f59ab50df9db22dae655a4597c
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-01-17 17:25:55 -0800

    fresh deck of cards

---
+++ deck.js
@@ -0,0 +1,23 @@
+const SUITS = ["♠", "♥", "♦", "♣"];
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+export default class Deck {
+    constructor(cards = freshDeck()){
+        this.cards = cards;
+    }
+}
+
+class Card {
+    constructor(suit, value){
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+function freshDeck(){
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+import Deck from "./deck.js";
+
+const deck = new Deck();
+console.log(deck.cards);
