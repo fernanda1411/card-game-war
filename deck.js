efe9b204d0327b3e759750963124b46e4c7aac23
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-01-17 20:25:44 -0800

    Card getHTML method

--- deck.js
+++ deck.js
@@ -25,6 +25,18 @@ class Card {
         this.suit = suit;
         this.value = value;
     }
+
+    get color(){
+        return this.suit === "♠" || this.suit === "♠" ? "black" : "red";
+    }
+
+    getHTML(){
+        const cardDiv = document.createElement("div");
+        cardDiv.innerText = this.suit;
+        cardDiv.classList.add("card", this.color);
+        cardDiv.dataset.value = `${this.value} ${this.suit}`;
+        return cardDiv;
+    }
 }
 
 function freshDeck(){
--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import Deck from "./deck.js";
 
+const computerCardSlot = document.querySelector('.computer-card-slot');
+const playerCardSlot = document.querySelector('.player-card-slot');
+
 const deck = new Deck();
 deck.shuffle();
-console.log(deck.cards);
+console.log(deck);
+
+computerCardSlot.appendChild(deck.cards[0].getHTML());
+playerCardSlot.appendChild(deck.cards[1].getHTML());
