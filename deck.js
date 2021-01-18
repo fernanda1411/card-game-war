5b30b5ff4471b84e83f1c91138f68cf7bc6677a9
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-01-17 18:17:48 -0800

    Deck shuffle

--- deck.js
+++ deck.js
@@ -5,6 +5,19 @@ export default class Deck {
     constructor(cards = freshDeck()){
         this.cards = cards;
     }
+
+    get numberOfCards(){
+        return this.cards.length;
+    }
+
+    shuffle(){
+        for (let i = this.numberOfCards - 1; i > 0; i--){
+            const newIndex = Math.floor(Math.random() * (i + 1));
+            const oldValue = this.cards[newIndex];
+            this.cards[newIndex] = this.cards[i];
+            this.cards[i] = oldValue;
+        }
+    }
 }
 
 class Card {
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Deck from "./deck.js";
 
 const deck = new Deck();
+deck.shuffle();
 console.log(deck.cards);
