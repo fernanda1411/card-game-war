389bf7dc868d50cef48e47b8448dec4f36d9c0d3
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-01-18 12:52:10 -0800

    Adding start game + cleanBeforeRound + updateDeckCount + cleaning index.html

--- script.js
+++ script.js
@@ -2,10 +2,36 @@ import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector('.computer-card-slot');
 const playerCardSlot = document.querySelector('.player-card-slot');
+const text = document.querySelector('.text');
+const playerDeckElement = document.querySelector('.player-deck');
+const computerDeckElement = document.querySelector('.computer-deck');
 
-const deck = new Deck();
-deck.shuffle();
-console.log(deck);
+let playerDeck, computerDeck;
 
-computerCardSlot.appendChild(deck.cards[0].getHTML());
-playerCardSlot.appendChild(deck.cards[1].getHTML());
+startGame();
+
+function startGame(){
+    const deck = new Deck();
+    deck.shuffle();
+
+    const deckMidpoint = Math.ceil(deck.numberOfCards / 2);
+    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
+    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
+    cleanBeforeRound();
+}
+
+function cleanBeforeRound(){
+    computerCardSlot.innerHTML = '';
+    playerCardSlot.innerHTML = '';
+    text.innerHTML = '';
+    updateDeckCount()
+}
+
+function updateDeckCount(){
+    computerDeckElement.innerText = computerDeck.numberOfCards;
+    playerDeckElement.innerText = playerDeck.numberOfCards;
+}
+
+
+// computerCardSlot.appendChild(deck.cards[0].getHTML());
+// playerCardSlot.appendChild(deck.cards[1].getHTML());
